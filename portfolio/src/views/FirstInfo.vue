efcf38aafcc5c06b9bf7be183a58415469def097
tg8685 <tg8685@gmail.com>
2022-06-16 18:08:26 +0900

    Feat: add portfolio 폴더

---
+++ portfolio/src/views/FirstInfo.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="total-frame">
+    <div class="total-info-box">
+      <div class="info-box col-6">
+        <h1 class="mb-4">Haesung Cho</h1>
+        <h5 class="mb-2">Frontend Developer.</h5>
+        <div class="introduction-content mt-1">
+          <!-- <strong> UX 개선</strong>과 <strong> 협업에 적극</strong>적인 -->
+          <!-- <strong> 프론트엔드 </strong> 개발자 조해성입니다. -->
+          <strong> 끝까지 해내는 집념을 가진 프론트엔드 </strong> 개발자
+          조해성입니다.
+          <!-- <strong>  인류발전에 프론트엔드 </strong> 개발자 -->
+          <!-- 조해성입니다. -->
+          <!-- <br />
+          <p style="font-size: 0.9rem">
+            개발자는 인류가 직면한 문제를 해결 할 수 있다고 믿습니다.
+          </p> -->
+        </div>
+        <!-- <p>
+        I believe developers are people who can solve the problems that humans
+        are experiencing.
+      </p> -->
+        <div class="subinfo-box">
+          <div>
+            <p><strong> E-mail</strong> : tg8685@gmail.com</p>
+          </div>
+          <div>
+            <p><strong> Phone</strong> : +82 10-4449-8685</p>
+          </div>
+          <div>
+            <p><strong> City</strong> : 부산, 대한민국</p>
+          </div>
+        </div>
+        <div
+          class="d-flex justify-content-start align-items-center link-images"
+        >
+          <a :href="links.facebook" target="_blank">
+            <img
+              class="link-image"
+              src="@/assets/facebook-square-brands.svg"
+              alt=""
+          /></a>
+          <a :href="links.github" target="_blank"
+            ><img
+              class="link-image"
+              src="@/assets/github-square-brands.svg"
+              alt=""
+          /></a>
+          <a :href="links.instagram" target="_blank"
+            ><img
+              class="link-image"
+              src="@/assets/instagram-square-brands.svg"
+              alt=""
+          /></a>
+          <a :href="links.linkedin" target="_blank"
+            ><img class="link-image" src="@/assets/linkedin-brands.svg" alt=""
+          /></a>
+        </div>
+      </div>
+      <div class="col d-flex justify-content-center">
+        <div class="image-box">
+          <img class="profile-image" src="@/assets/profile/imoji1.png" alt="" />
+          <!-- <img class="profile-image" src="@/assets/profile/motorcycleAndMe.jpg" alt="" /> -->
+          <!-- <img class="profile-image" src="@/assets/profile/profileImage.jpg" alt="" /> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { reactive } from "vue";
+export default {
+  name: "FirstInfo",
+  setup() {
+    const links = reactive({
+      facebook: "https://www.facebook.com/tanggae",
+      github: "https://github.com/daedaem",
+      instagram: "https://www.instagram.com/sun_castle_8685/",
+      linkedin:
+        "https://www.linkedin.com/in/%ED%95%B4%EC%84%B1-%EC%A1%B0-518a021a9/",
+    });
+    return { links };
+  },
+};
+</script>
+<style lang="scss">
+h1 {
+  font-size: 4rem;
+  font-weight: bold;
+  font-family: "SF Pro Display", -apple-system, system-ui, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+}
+h2 {
+  font-size: 3.5rem;
+}
+h3 {
+  font-size: 3rem;
+  font-weight: bold;
+  font-family: "SF Pro Display", -apple-system, system-ui, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+}
+h4 {
+  font-size: 2rem;
+}
+h5 {
+  font-size: 1.5rem;
+}
+a {
+  color: black;
+  // font-size: 1rem;
+  text-decoration: none;
+}
+
+.total-info-box {
+  max-width: 1000px;
+  height: 90vh;
+  margin: 0rem auto 0rem auto;
+  display: flex;
+  align-items: center;
+}
+.info-box {
+  text-align: start;
+  padding: 0rem 0rem 0rem 3rem;
+  // background: #fafafa;
+}
+.introduction-content {
+  text-align: start;
+  // align-items: center;
+  // padding: 1rem 4rem 1rem 1rem;
+}
+.subinfo-box {
+  margin: 2rem 0rem 0rem 0rem;
+}
+
+.image-box {
+  // background: whitesmoke;
+  // align-items: center;
+  width: 350px;
+  height: 350px;
+
+  border-radius: 100%;
+  overflow: hidden;
+  // width: 400px;
+}
+.profile-image {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+
+  // width: 400px;
+}
+.link-images {
+  gap: 1rem;
+}
+.link-image {
+  width: 2rem;
+}
+</style>
---
+++ portfolio/src/views/Skills.vue
@@ -0,0 +1,206 @@
+<template>
+  <div class="total-skills-box">
+    <div class="skill-content-box">
+      <h3 class="pt-5">SKILL</h3>
+      <p class="fs-5">보유 기술 스택</p>
+      <div class="hr-1"></div>
+      <div class="skill-boxes">
+        <div class="skill-box">
+          <p class="fs-5 mb-0 fw-bold">HTML, CSS, Javascript</p>
+          <img
+            class="skill-logo-image"
+            src="@/assets/htmlJsCss.png"
+            style="width: 220px"
+            alt=""
+          />
+          <div class="mt-4">
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star"></i>
+          </div>
+          <p class="text-start px-4 pt-4" style="font-size: 0.9rem">
+            계획된 와이어 프레임과 동일한 웹 구현
+            <br />
+            / 반응형 설계 가능
+          </p>
+        </div>
+        <div class="skill-box">
+          <p class="fs-5 mb-0 fw-bold">Vue.js</p>
+
+          <img
+            class="skill-logo-image"
+            src="@/assets/VueLogo.png"
+            style="width: 5rem"
+            alt=""
+          />
+          <div class="mt-4">
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+          </div>
+          <p class="text-start px-4 pt-4" style="font-size: 0.9rem">
+            vue 2.x 및 3.x 버젼 사용 가능 /
+            <br />
+            vuex 활용한 상태관리를 이해하고 활용 /
+            <br />
+            작은 규모의 웹서비스 구현 가능
+          </p>
+          <!-- <p>아아</p> -->
+        </div>
+        <div class="skill-box">
+          <p class="fs-5 mb-0 fw-bold">Python</p>
+          <img
+            class="skill-logo-image"
+            src="https://raw.githubusercontent.com/pkkulhari/pkkulhari/master/icons/python.svg"
+            style="width: 5rem"
+            alt=""
+          />
+          <div class="mt-4">
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+          </div>
+          <p class="text-start px-4 pt-4" style="font-size: 0.9rem">
+            정렬, 시물레이션, 그래프 탐색 등 기본 알고리즘 구현
+            <br />
+            <!-- / 삼성 SW 역량테스트 IM 취득 -->
+          </p>
+          <!-- <p>아아</p> -->
+        </div>
+        <div class="skill-box">
+          <p class="fs-5 mb-0 fw-bold">Git</p>
+
+          <img
+            class="skill-logo-image"
+            src="https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667"
+            style="width: 6rem"
+            alt=""
+          />
+          <div class="mt-4">
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+          </div>
+          <p class="text-start px-4 pt-4" style="font-size: 0.9rem">
+            Gitflow branch 전략을 활용한 협업 가능 <br />
+            / Git convention 숙지하여 협업 가능
+          </p>
+        </div>
+        <div class="skill-box">
+          <p class="fs-5 mb-0 fw-bold">Django</p>
+
+          <img
+            class="skill-logo-image"
+            src="@/assets/django.png"
+            style="width: 6rem"
+            alt=""
+          />
+          <div class="mt-4">
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+          </div>
+          <p class="text-start px-4 pt-4" style="font-size: 0.9rem">
+            CRUD 기능 구현 가능 / Django REST Framework를 활용한 API 설계 구현
+          </p>
+          <!-- <p>아아</p> -->
+        </div>
+        <div class="skill-box">
+          <p class="fs-5 mb-0 fw-bold">React</p>
+
+          <img
+            class="skill-logo-image"
+            src="@/assets/react.png"
+            style="width: 100px"
+            alt=""
+          />
+          <div class="mt-4">
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star-fill"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+            <i class="bi bi-star"></i>
+          </div>
+          <p class="text-start px-4 pt-4" style="font-size: 0.9rem">
+            CRUD 기능 구현 가능 <br />
+            / 현재 학습 진행 중
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Experiences",
+  setup() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped>
+.total-skills-box {
+  //   height: 100vh;
+  max-width: 1000px;
+  //   background: #fafafa;
+  margin: 0rem auto 5rem auto;
+  //   width: 99vw;
+}
+.skill-content-box {
+  //   max-width: 1200px;
+  margin: 0 auto 0 auto;
+}
+.hr-1 {
+  width: 10%;
+  margin: 1rem auto 2rem auto;
+  //   padding: 0rem auto 0rem auto;
+  //   color: rgb(0, 0, 0, 0.4);
+  border-bottom: 1px solid white;
+}
+.skill-boxes {
+  max-width: 980px;
+  min-height: 550px;
+  margin: 0rem auto 0rem auto;
+  display: grid;
+  grid-gap: 2rem;
+
+  grid-template-columns: repeat(3, 1fr);
+}
+.skill-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  height: 100%;
+  background: radial-gradient(145% 145.46% at 0% 0%, #fff 0%, #eee 100%);
+  box-shadow: 20px 20px 60px rgb(0 0 0 / 5%),
+    -20px -20px 60px rgb(255 255 255 / 80%);
+  border-radius: 20px;
+  //   margin: 0 auto 0 auto;
+  padding: 2rem 0rem 1rem 0rem;
+  //   max-width: 350px;
+}
+.skill-logo-image {
+  margin: 1rem 0 0rem 0;
+}
+.bi-star-fill {
+  color: #fe8a01;
+  font-size: 0.8rem;
+  margin: 0rem 0.25rem 1rem 0.25rem;
+}
+.bi-star {
+  color: #fe8a01;
+  font-size: 0.8rem;
+  margin: 0rem 0.25rem 1rem 0.25rem;
+}
+</style>
