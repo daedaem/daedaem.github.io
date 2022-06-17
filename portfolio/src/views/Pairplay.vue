ee7b9a11ad9671a97565b7e285f2dd6555bbd6da
tg8685 <tg8685@gmail.com>
2022-06-17 16:39:19 +0900

    Feat: update 포트폴리오

---
+++ portfolio/src/views/Pairplay.vue
@@ -0,0 +1,187 @@
+<template>
+  <div class="total-info-box">
+    <div class="info-box">
+      <h1 class="my-5">Pairplay</h1>
+      <div class="portfolio-detail-info-box">
+        <h5>Info</h5>
+        <div class="portfolio-detail-info-titles col-1">
+          <div class="portfolio-detail-info-title">개발 기간</div>
+          <div class="portfolio-detail-info-title">서비스</div>
+          <div class="portfolio-detail-info-title">기술 스택</div>
+          <div class="portfolio-detail-info-title">관련 링크</div>
+          <div class="portfolio-detail-info-title">담당 업무</div>
+        </div>
+        <div class="portfolio-detail-info-contents col">
+          <div class="portfolio-detail-info-content">
+            2022.04.11 ~ 2022.05.20
+          </div>
+          <div class="portfolio-detail-info-content">
+            전국 체육시설 조회, 예약 및 운동 메이트 중개 플랫폼
+          </div>
+          <div class="portfolio-detail-info-content">
+            Javascript , Vue 3, Scss
+          </div>
+          <div class="portfolio-detail-info-content">
+            <a href="https://pairplay.site/" target="_blan`k">
+              https://pairplay.site/</a
+            ><br />
+          </div>
+          <div class="portfolio-detail-info-content">
+            <strong> 운동장소 메인페이지</strong> <br />
+            - 운동 카테고리 선택, 회원 주소지 내 인기시설, 최근 조회한 시설 제공
+            <br />
+            <br />
+            <strong> 운동장소 검색 페이지 </strong>
+            <br />
+            - 지역, 운동 종목, 사용 날짜 필터링 및 검색 기능 <br />- Naver Map
+            api 활용한 위치기반 운동 시설 정보 제공
+            <br />
+            <br />
+            <strong> 운동장소 상세페이지</strong> <br />- 시설 상세정보(평점,
+            이용시간 및 금액, 시설정보, 위치, 이용 규칙, 리뷰 등) 제공 <br />-
+            날짜별 이용 예약가능 시간 조회 및 아임포트 API를 활용한 예약기능<br />
+          </div>
+        </div>
+      </div>
+      <!-- <p>
+        I believe developers are people who can solve the problems that humans
+        are experiencing.
+      </p> -->
+      <h5 class="my-5">View</h5>
+      <div class="container portfolio-detail-image-box">
+        <div class="row">
+          <div class="col-4">
+            <img
+              class="portfolio-detail-image"
+              src="@/assets/pairplay/pairplayPlaceMain.png"
+              alt=""
+            />
+          </div>
+          <div class="col-4">
+            <img
+              class="portfolio-detail-image"
+              src="@/assets/pairplay/pairplayPlaceDetail.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="col-4">
+          <!-- <img
+            class="portfolio-detail-image"
+            src="@/assets/pairplay/장소수정.gif"
+            alt=""
+          /> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { reactive } from "vue";
+export default {
+  name: "Pairplay",
+  setup() {
+    const links = reactive({
+      facebook: "https://www.facebook.com/tanggae",
+      github: "https://github.com/daedaem",
+      instagram: "https://www.instagram.com/sun_castle_8685/",
+      linkedin:
+        "https://www.linkedin.com/in/%ED%95%B4%EC%84%B1-%EC%A1%B0-518a021a9/",
+    });
+    return { links };
+  },
+};
+</script>
+<style lang="scss">
+h1 {
+  font-size: 4rem;
+  font-weight: bold;
+  font-family: "SF Pro Display", -apple-system, system-ui, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+}
+h2 {
+  font-size: 3.5rem;
+}
+h3 {
+  font-size: 3rem;
+  font-weight: bold;
+  font-family: "SF Pro Display", -apple-system, system-ui, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+}
+h4 {
+  font-size: 2rem;
+}
+h5 {
+  font-size: 1.5rem;
+}
+a {
+  color: black;
+  // font-size: 1rem;
+  text-decoration: none;
+}
+
+.total-info-box {
+  max-width: 1000px;
+  //   height: 94vh;
+  margin: 0rem auto 0rem auto;
+  //   display: flex;
+  align-items: center;
+}
+.info-box {
+  text-align: start;
+  padding: 0rem 0rem 0rem 3rem;
+  // background: #fafafa;
+}
+.portfolio-detail-info-title {
+  margin: 0rem 0rem 1rem 0rem;
+  font-weight: bold;
+  color: rgb(50, 49, 49);
+}
+.portfolio-detail-info-content {
+  margin: 0rem 0rem 1rem 0rem;
+}
+.portfolio-detail-info-box {
+  display: flex;
+  gap: 4rem;
+  // align-items: center;
+  // padding: 1rem 4rem 1rem 1rem;
+}
+.subinfo-box {
+  margin: 2rem 0rem 0rem 0rem;
+}
+
+.image-box {
+  // background: whitesmoke;
+  // align-items: center;
+  width: 350px;
+  height: 350px;
+
+  border-radius: 100%;
+  overflow: hidden;
+  // width: 400px;
+}
+.profile-image {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+
+  // width: 400px;
+}
+
+.link-image {
+  width: 2rem;
+}
+// .portfolio-detail-image-box {
+//   display: grid;
+
+//   //   grid-template-columns: repeat(1, 5fr);
+// }
+.container.portfolio-detail-image-box {
+  gap: 1rem;
+}
+.portfolio-detail-image {
+  max-width: 300px;
+  //   min-height: 250px;
+  //   object-fit: cover;
+}
+</style>
---
+++ portfolio/src/views/Portfolio.vue
@@ -0,0 +1,246 @@
+<template>
+  <div class="total-portfolio-box">
+    <div class="portfolio-content-box">
+      <h3 class="pt-5">PORTFOLIO</h3>
+      <!-- <p class="fs-5">보유 기술 및 사용 언어</p> -->
+      <div class="hr-1"></div>
+      <div class="portfolio-boxes">
+        <div class="portfolio-box text-start">
+          <div class="portfolio-box-content col-4">
+            <!-- <p class="fs-5 mb-0 fw-bold">Pairplay</p> -->
+            <img
+              class="portfolio-logo-image me-auto"
+              src="@/assets/pairplay/PairPlayLogo.png"
+              style="width: 200px"
+              alt=""
+            />
+            <p></p>
+            <div class="portfolio-content-description">
+              전국 체육시설 조회, 예약 및 운동 메이트 중개 플랫폼
+            </div>
+            <div class="mt-4">2022.04.11 ~ 2022.05.20</div>
+            <!-- <div class="my-3 skilbutton"> -->
+            <!-- <img v-for="link in logos" :key="link.id" :src="`${link}`" /> -->
+            <!-- </div> -->
+            <a href="https://github.com/daedaem/Pairplay" target="_blank">
+              <button type="button" class="btn btn-dark mt-5">
+                저장소로 이동 <i class="bi bi-arrow-right-short"></i>
+              </button>
+            </a>
+          </div>
+          <div>
+            <img
+              class="portfolio-box-image"
+              style="width: 400px"
+              src="@/assets/pairplay/pairplayMain.png"
+              alt=""
+            />
+          </div>
+          <!-- <p>아아</p> -->
+        </div>
+        <div class="portfolio-box text-start">
+          <div class="portfolio-box-content col-4">
+            <!-- <p class="fs-5 mb-0 fw-bold">Pairplay</p> -->
+            <img
+              class="portfolio-logo-image me-auto"
+              src="@/assets/unique/UniqueLogo.png"
+              style="width: 150px"
+              alt=""
+            />
+            <p></p>
+            <div class="portfolio-content-description">
+              NFT 민팅, 거래 및 전시회 플랫폼
+            </div>
+            <div class="mt-4">2022.02.28 ~ 2022.04.08</div>
+            <!-- <div class="my-3 skilbutton"> -->
+            <!-- <img v-for="link in logos" :key="link.id" :src="`${link}`" /> -->
+            <!-- </div> -->
+            <a
+              href="https://github.com/daedaem/Unique-NFT_MarketExhibition"
+              target="_blank"
+            >
+              <button type="button" class="btn btn-dark mt-5">
+                저장소로 이동 <i class="bi bi-arrow-right-short"></i>
+              </button>
+            </a>
+          </div>
+          <div>
+            <img
+              class="portfolio-box-image"
+              style="width: 400px"
+              src="@/assets/unique/uniqieMain.png"
+              alt=""
+            />
+          </div>
+          <!-- <p>아아</p> -->
+        </div>
+        <div class="portfolio-box text-start">
+          <div class="portfolio-box-content col-4">
+            <!-- <p class="fs-5 mb-0 fw-bold">Pairplay</p> -->
+            <img
+              class="portfolio-logo-image me-auto"
+              src="@/assets/campus/campusLogo.png"
+              style="width: 150px"
+              alt=""
+            />
+            <p></p>
+            <div class="portfolio-content-description">
+              캠핑 SNS, 캠핑시설 조회, 캠핑 메이트 중개 서비스
+            </div>
+            <div class="mt-4">2022.01.04 ~ 2022.02.18</div>
+            <!-- <div class="my-3 skilbutton"> -->
+            <!-- <img v-for="link in logos" :key="link.id" :src="`${link}`" /> -->
+            <!-- </div> -->
+            <a href="https://github.com/daedaem/Campus" target="_blank">
+              <button type="button" class="btn btn-dark mt-5">
+                저장소로 이동 <i class="bi bi-arrow-right-short"></i>
+              </button>
+            </a>
+          </div>
+
+          <div>
+            <img
+              class="portfolio-box-image"
+              style="width: 400px"
+              src="@/assets/campus/campusMain.png"
+              alt=""
+            />
+          </div>
+          <!-- <p>아아</p> -->
+        </div>
+        <div class="portfolio-box text-start">
+          <div class="portfolio-box-content col-4">
+            <!-- <p class="fs-5 mb-0 fw-bold">Pairplay</p> -->
+            <img
+              class="portfolio-logo-image me-auto"
+              src="@/assets/moving/MovingLogo.png"
+              style="width: 170px"
+              alt=""
+            />
+            <p></p>
+            <div class="portfolio-content-description">
+              영화 정보 기반 추천 서비스
+            </div>
+            <div class="mt-4">2021.11.17 ~ 2021.11.25</div>
+            <!-- <div class="my-3 skilbutton"> -->
+            <!-- <img v-for="link in logos" :key="link.id" :src="`${link}`" /> -->
+            <!-- </div> -->
+            <a
+              href="https://github.com/daedaem/Moving_SSAFY-1th-Project"
+              target="_blank"
+            >
+              <button type="button" class="btn btn-dark mt-5">
+                저장소로 이동 <i class="bi bi-arrow-right-short"></i>
+              </button>
+            </a>
+          </div>
+
+          <div>
+            <img
+              class="portfolio-box-image"
+              style="width: 400px"
+              src="@/assets/moving/MovingMain.png"
+              alt=""
+            />
+          </div>
+          <!-- <p>아아</p> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+// import { reactive } from "@vue/reactivity";
+export default {
+  name: "Portfolio",
+  setup() {
+    // const logos = reactive([]);
+    // return { logos };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.total-portfolio-box {
+  //   height: 100vh;
+  max-width: 1000px;
+  //   background: #fafafa;
+
+  margin: 5rem auto 5rem auto;
+
+  //   width: 99vw;
+}
+.portfolio-content-box {
+  //   max-width: 1200px;
+  //   padding: rem 0rem 0rem 0rem;
+  margin: 1rem auto 0 auto;
+}
+.hr-1 {
+  width: 20%;
+  margin: 1rem auto 2rem auto;
+  //   padding: 0rem auto 0rem auto;
+  //   color: rgb(0, 0, 0, 0.4);
+  border-bottom: 1px solid white;
+}
+.portfolio-boxes {
+  max-width: 980px;
+
+  margin: 0rem auto 0rem auto;
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(1, 1fr);
+}
+.portfolio-box {
+  //   min-height: 350px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 100%;
+  background: radial-gradient(145% 145.46% at 0% 0%, #fff 0%, #eee 100%);
+  box-shadow: 20px 20px 60px rgb(0 0 0 / 5%),
+    -20px -20px 60px rgb(255 255 255 / 80%);
+  border-radius: 20px;
+  //   margin: 0 auto 0 auto;
+  padding: 1.3rem 1.3rem 1.3rem 1.3rem;
+  //   max-width: 350px;
+}
+// .portfolio-box-image{
+
+// }
+.portfolio-box-content {
+  //   display: flex;
+  //   flex-direction: column;
+  //   align-items: stretch;
+  margin: 1.5rem 0rem 1.5rem 0rem;
+}
+// .portfolio-logo-image {
+//   margin: 0rem 0 0rem 0;
+// }
+.portfolio-content-description {
+  font-weight: bold;
+  font-size: 1.1rem;
+}
+.skilbutton {
+  //   display: g
+  //   grid-gap: rem;
+  grid-template-columns: repeat(3, 1fr);
+}
+.bi-star-fill {
+  color: #fe8a01;
+  font-size: 0.8rem;
+  margin: 0rem 0.25rem 1rem 0.25rem;
+}
+.btn.btn-dark {
+  border-radius: 24px;
+  width: 10rem;
+  font-weight: bold;
+  font-size: 0.8rem;
+  padding: 0.75rem 1.5rem;
+  background: #363636;
+  box-shadow: -4px -4px 10px rgb(255 255 255 / 80%),
+    4px 4px 10px rgb(0 0 0 / 20%);
+  & a {
+    text-decoration: none;
+    color: white;
+  }
+}
+</style>
